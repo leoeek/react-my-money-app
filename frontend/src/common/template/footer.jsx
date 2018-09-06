aef2b15fa9d64a8934fd21a7e31588b382f95d02
Unknown <leonardolp@gmail.com>
2018-09-06 17:41:01 -0300

    Iniciando

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2018 <a href="http://apisystems.com.br/" target="_blank">API</a>
+        </strong>
+    </footer>
+)
